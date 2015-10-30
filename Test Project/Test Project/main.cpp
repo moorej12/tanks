af2b4a9f3ad39e41a362c9cd0a2ebc5368ff27c8
ScottGWalter <ScottW@SWalter-2.local>
2015-10-29 19:13:14 -0600

    Scott Test

---
+++ Test Project/Test Project/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Test Project
+//
+//  Created by scott walter on 2015-10-29.
+//  Copyright © 2015 scott walter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+// This was created by Scott as a test
